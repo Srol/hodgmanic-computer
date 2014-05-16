666c880ff2998142ec424b5e7f0a91a86c2c7956
Patrick Hogan <srol.hogan@gmail.com>
2014-05-16 10:37:02 -0400

    Thermal printer works fine with printing individual names, but printing all isn't working. Hopefully this loop fixes that.

--- hodgprint.py
+++ hodgprint.py
@@ -11,16 +11,18 @@ yn = ""
 if hobonumber == "ALL":
 	hoboinfo = open("hobocleaned.txt", "r")
 	p = printer.ThermalPrinter(serialport="/dev/ttyAMA0")
-	yn = raw_input("Are you sure you want to print all 700 hobo names y/n? > ")
+	print hoboinfo.read()
+	yn = raw_input("Are you sure you want a hard copy of all 700 hobo names y/n? > ")
 	if yn.upper() == "Y":
-		print hoboinfo.read()
-		p = printer.ThermalPrinter(serialport="/dev/ttyAMA0")
-		wrapped_text = textwrap.fill(hoboinfo, 32)
-		p.print_text(wrapped_text)
+		for line in hoboinfo:		
+			p = printer.ThermalPrinter(serialport="/dev/ttyAMA0")
+			wrapped_text = textwrap.fill(line, 32)
+			p.print_text(wrapped_text)
 		p.linefeed() 
 		p.linefeed() 
 		p.linefeed()
-		hoboinfo.close()
+	else:
+		print "OPERATION TERMINATED AT USER REQUEST THAT IS ALL"
 elif int(hobonumber) < 701 :
 	hoboinfo = linecache.getline("hobocleaned.txt", int(hobonumber))
 	print hoboinfo
