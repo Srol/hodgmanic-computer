129a92e742fd22c2c3925e14361319c042ef0300
Patrick Hogan <srol.hogan@gmail.com>
2014-05-16 09:51:28 -0400

    Add Thermal Printer capability
    Hodgprint.py now delivers a hobo name, then sends it to the thermal
    printer. Requires the thermal printer library. HAS NOT BEEN TESTED WITH
    THERMAL PRINTER YET.

--- hodgprint.py
+++ hodgprint.py
@@ -1,19 +1,37 @@
-# import printer
+import printer
 import textwrap
 import linecache
 
 hobonumber = raw_input("Please enter hobo # > ")
 hobonumber = hobonumber.upper()
 hoboinfo = ""
+wrapped_text = ""
+yn = ""
 
 if hobonumber == "ALL":
 	hoboinfo = open("hobocleaned.txt", "r")
-	print hoboinfo.read()
-	hoboinfo.close()
+	p = printer.ThermalPrinter(serialport="/dev/ttyAMA0")
+	yn = raw_input("Are you sure you want to print all 700 hobo names y/n? > ")
+	if yn.upper() = "Y":
+		print hoboinfo.read()
+		p = printer.ThermalPrinter(serialport="/dev/ttyAMA0")
+		wrapped_text = textwrap.fill(hoboinfo, 32)
+		p.print_text(wrapped_text)
+		p.linefeed() 
+		p.linefeed() 
+		p.linefeed()
+		hoboinfo.close()
+	else:
+		break
 elif int(hobonumber) < 701 :
 	hoboinfo = linecache.getline("hobocleaned.txt", int(hobonumber))
 	print hoboinfo
+	p = printer.ThermalPrinter(serialport="/dev/ttyAMA0")
+	wrapped_text = textwrap.fill(hoboinfo, 32)
+	p.print_text(wrapped_text)
+	p.linefeed() 
+	p.linefeed() 
+	p.linefeed()
+	hoboinfo.close()
 else:
 	print "INVALID HOBO!"
-
-# p = printer.ThermalPrinter(serialport="/dev/ttyAMA0")
