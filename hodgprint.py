0368b9552f8c1588e548c07bb8701bc5170b6338
Patrick Hogan <srol.hogan@gmail.com>
2014-05-16 11:01:33 -0400

    trying to fix print all again.

--- hodgprint.py
+++ hodgprint.py
@@ -1,4 +1,4 @@
-import printer
+#import printer
 import textwrap
 import linecache
 
@@ -7,31 +7,31 @@ hobonumber = hobonumber.upper()
 hoboinfo = ""
 wrapped_text = ""
 yn = ""
+p = printer.ThermalPrinter(serialport="/dev/ttyAMA0")
 
 if hobonumber == "ALL":
 	hoboinfo = open("hobocleaned.txt", "r")
-	p = printer.ThermalPrinter(serialport="/dev/ttyAMA0")
-	print hoboinfo.read()
+	lines = hoboinfo.read().splitlines()
 	yn = raw_input("Are you sure you want a hard copy of all 700 hobo names y/n? > ")
 	if yn.upper() == "Y":
-		for line in hoboinfo:		
-			p = printer.ThermalPrinter(serialport="/dev/ttyAMA0")
+		for line in lines:		
+			print line
 			wrapped_text = textwrap.fill(line, 32)
 			p.print_text(wrapped_text)
-		p.linefeed() 
-		p.linefeed() 
-		p.linefeed()
+			p.linefeed() 
+			p.linefeed() 
+			p.linefeed()
 	else:
 		print "OPERATION TERMINATED AT USER REQUEST THAT IS ALL"
 elif int(hobonumber) < 701 :
 	hoboinfo = linecache.getline("hobocleaned.txt", int(hobonumber))
 	print hoboinfo
-	p = printer.ThermalPrinter(serialport="/dev/ttyAMA0")
 	wrapped_text = textwrap.fill(hoboinfo, 32)
 	p.print_text(wrapped_text)
 	p.linefeed() 
 	p.linefeed() 
 	p.linefeed()
-	hoboinfo.close()
 else:
 	print "INVALID HOBO!"
+	
+hoboinfo.close()
